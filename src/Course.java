9bd3442167808fbfb558d190334b6fcecc9abf0c
omar emad <omaremad11113@gmail.com>
2025-04-19 17:17:57 +0200

    CourseClass

--- src/Course.java
+++ src/Course.java
@@ -1,3 +1,28 @@
-public class Course {
-//
+class Course{
+
+    public String CourseName;
+    public int CreditHours;
+    public Double Grade = 0.0;
+    
+    Course(String CourseName, int CreditHours){
+        this.CourseName = CourseName;
+        this.CreditHours = CreditHours;
+    }
+    
+    public void Set_Name(String CourseName){
+        this.CourseName = CourseName;
+    }
+    
+    public void Set_Credits(int CreditHours){
+        this.CreditHours = CreditHours;
+    }
+    
+    public void Set_Grades(Double Grade){
+        this.Grade = Grade;
+    }
+    
+    public Double Get_Grades(){
+        return Grade;
+    }
+    
 }
