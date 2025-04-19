2cfd09835194a9ec27270a1e9d890eeb8e93fd68
OmarHesham2356 <152777160+OmarHesham2356@users.noreply.github.com>
2025-04-19 18:33:43 +0200

    ReportCard_Finished

--- src/ReportCard.java
+++ src/ReportCard.java
@@ -16,7 +16,12 @@ public class ReportCard {
         student.Get_Faculty();
         student.Get_ID();
         student.Calculate_GPA();
-        
+        //Display
+        System.out.println("Student Name:"+ student.Get_Name());
+        System.out.println("Student ID:"+ student.Get_ID());
+        System.out.println("Faculty:"+ student.Get_Faculty());
+        System.out.println("Courses:"+ student.Get_Courses());
+        System.out.println("GPA:"+ student.Get_GPA());
     }	
 
 }
