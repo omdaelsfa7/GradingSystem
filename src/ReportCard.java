d050f555629d3704f1612a6decb1f1e48fa6abdc
OmarHesham2356 <152777160+OmarHesham2356@users.noreply.github.com>
2025-04-19 17:33:20 +0200

    i added ReportClass file with constructors and method

---
+++ src/ReportCard.java
@@ -0,0 +1,22 @@
+import java.util.jar.Attributes.Name;
+
+public class ReportCard  {
+
+    String Students;
+    
+
+    ReportCard(String Students){
+        
+        this.Students = Students;
+
+        
+
+    }
+
+    public void generateReportCard(){
+
+        Students.Get_Name
+        Students.Get_Courses
+    }	
+
+}
